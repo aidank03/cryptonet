2b9ab4afc1fa4a4c5b8218be9c1e836883af4bd6
po <apolloheidal@gmail.com>
2018-01-03 00:47:49 -0800

    jan 2

--- CryptoNet.py
+++ CryptoNet.py
@@ -3,7 +3,7 @@ import time, datetime, math
 import tensorflow as tf
 import numpy as np
 
-time0 = time.time()
+start_time = time.time()
 
 secret = '782c06450cacda6c91c5f6ea2842e7c70b1a86889721922c711c749dd86be9819ee3e1264fa6a3bfdc0f90660629ecef88182da3788e9cee0831cf1f1b8e32a8'
 key = 'WRO9ZNTF-4H6TUE1Q-G50LJUF0-JH6JV4SH'
@@ -12,98 +12,130 @@ key = 'WRO9ZNTF-4H6TUE1Q-G50LJUF0-JH6JV4SH'
 
 def main():
 	batch = createBatch()
-	print(batch[0][0],batch[-1][0])
+	time0 = time.time()
+	print('batch = [time, total, rate, type]')
+	print(batch[10:])
+	print('batch shape:',batch.shape)
+	print('createBatch time:',time0 - start_time,'\n')
 
-	input_nodes = 300
-	output_nodes = 20
-	labels = np.empty(1)
+
+	o_nodes = 30
+	labels = createLabels(batch,o_nodes)
+	time1 = time.time()
+	print('labels length:',len(labels))
+	print('createLabels time:',time1 - start_time)
+
+
+	i_nodes = 180
+	training_data = createTrainingData(batch, labels, i_nodes)
+	time2 = time.time()
+	print(training_data[:5])
+	print('training_data shape:',training_data.shape)
+	print('createTrainingData time:',time2 - start_time,'\n')
+	
+
+	'''tfGraph(training_data,labels,i_nodes,o_nodes)'''
 
 
 # create array filled with relevant info (oldest to newest)
 def createBatch():
 	pt = polonitrade.Poloniex()
 	cP = 'USDT_BTC'
-	batches = 2
-	batch = np.empty([50000 * batches])
+	batches = 1
+	batch = np.empty([50000 * batches,4])
 
 	batch_time = time.time()
 	for b in range(batches):
 		next_batch = pt.marketTradeHist(currencyPair=cP,start=0,end=batch_time)
 		batch_time = int(time.mktime(datetime.datetime.strptime(next_batch[-1]['date'], "%Y-%m-%d %H:%M:%S").timetuple()))
-		for n in next_batch:
-			transaction = next_batch[-1 - n]
+		for n in range(len(next_batch)):
+			transaction = next_batch[n]
 			t_time = time.mktime(datetime.datetime.strptime(transaction['date'], "%Y-%m-%d %H:%M:%S").timetuple())
 			total = float(transaction['total'])
-			price = float(transaction['rate'])
+			rate = float(transaction['rate'])
 			order_type = transaction['type']
 			if order_type == 'buy':
-				order_type = -1.0
-			else:
 				order_type = 1.0
-			info = np.array([t_time,total,price,order_type])
-			batch[-1 - ((b * 50000) + n)] = info
-
+			else:
+				order_type = -1.0
+			reverse = -1 - ((b * 50000) + n)
+			batch[reverse][0] = t_time
+			batch[reverse][1] = total
+			batch[reverse][2] = rate
+			batch[reverse][3] = order_type
 	return batch
 
 
-'''
-
-# reverse order to account for newest order being first in batch
-
-# prepare training_data and labels
-	start_time = batch_time
-	time_diff =
-
-	for t in range(len(batch) - input_nodes):
-		transaction_time = int(time.mktime(datetime.datetime.strptime(batch[t]['date'], "%Y-%m-%d %H:%M:%S").timetuple()))
-		for x in range(output_nodes):
-
-
-		t_current = batch[t]
-		total = math.log1p(float(current['total']))
-		price = float(current['rate'])
-		price_diff = []
-		for n in range(input_nodes):
-			n_current = batch[t + n]
-			n_next_price = float(n_current['rate'])
-			price_diff_of_next = abs(price - n_next_price)
-			munge = math.log1p(total * math.log1p(price_diff_of_next))
-			price_diff.append(munge)
-		training_data.append(price_diff)
-	print(len(training_data))
-
-# prepare labels
-
-for el in training_data:
-	price = float(el['rate'])
-	runningMarketValue += price
-	orderType = el['type']
-	if orderType == 'buy':
-		order = {'amount': el['amount'], 'price': price}
-		print('Buy',order['amount'],cP,'@',order['price'])
-		buyList.append(order)
-	if orderType == 'sell':
-		order = {'amount': el['amount'], 'price': price}
-		print('Sell',order['amount'],cP,'@',order['price'])
-		sellList.append(order)
-
-print(len(training_data))
-print(training_data[0],training_data[-1])
-print('Market:',runningMarketValue,'USD')
+def createLabels(batch,o_nodes):
+	labels = {}
+	temp = {}
+	for t in batch:
+		t_time = t[0]
+		t_rate = t[2]
+		if t_time in labels:
+			if t_time in temp:
+				temp[t_time][0] += 1
+				temp[t_time][1] += t_rate
+			else:
+				temp[t_time] = [1,t_rate]
+		else:
+			labels[t_time] = t_rate
+	for key in sorted(temp):
+		avg_rate_from_temp = temp[key][1] / (temp[key][0])
+		labels[key] = avg_rate_from_temp
+	return labels
 
 
-sess = tf.Session()
-x = tf.placeholder(tf.float32,[None, ])
+def createTrainingData(batch, labels, i_nodes):
+	batch_length = batch.shape[0]
+	training_data = np.empty([batch_length,i_nodes + 1])
 
-print('Buy:',*buyList, sep='\n')
-print('\n')
-print('Sell:',*sellList, sep='\n')
+	for current_t in range(batch_length - i_nodes):
+		training_data[current_t][0] = batch[current_t + i_nodes][0]
+		rate_diff = np.empty([i_nodes])
+		transaction = batch[current_t + i_nodes]
+		log_total = math.log1p(transaction[1])
+		for previous_t in range(i_nodes):
+			p = batch[current_t - previous_t]
+			log_rate_diff = math.log1p(abs(transaction[2] - p[2]))
+			munge = math.log1p(log_total * log_rate_diff)
+			if p[3] < 0.0:
+				munge *= -1.0
+			training_data[current_t][previous_t + 1] = munge
+	return training_data
 
-t1 = time.time() - t0
-time.sleep(60 - t1)
-'''
+# not complete
+def tfGraph(training_data,labels,i_nodes,o_nodes):
+	sess = tf.Session()
+	x = tf.placeholder(tf.float32,shape=[None, i_nodes])
+	y_ = tf.placeholder(tf.float32, shape=[None, o_nodes])
+
+	W = tf.Variable(tf.zeros([i_nodes,o_nodes]))
+	b = tf.Variable(tf.zeros([o_nodes]))
+
+	sess.run(tf.global_variables_initializer())
+
+	y = tf.matmul(x,W) + b
+
+	cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+
+	for cycle in range(100):
+  		train_batch = training_data[cycle * 100:(cycle+1) *100][1:]
+		label_batch = labels[training_data[cycle * 100:(cycle+1) *100][0]]
+  		train_step.run(feed_dict={x: train_batch, y_: label_batch})
+
+	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+
 
 main()
 
-time1 = time.time()
-print(time1 - time0)
+end_time = time.time()
+print('run time:',end_time - start_time)
